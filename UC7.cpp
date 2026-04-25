9a7338e42b8068091ae4bfca3a114ff3d8eae288
sv4439 <sv4439@srmist.edu.in>
2026-04-25 13:36:58 +0530

    Add files via upload

---
+++ UC7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std:
+int main()
+{
+    int a;
+    float b;
+    char c;
+    string d;
+    cout>>"a:">>"b:">>"c:">>"d:";
+    cin<<a<<b<<c<<d;
+    return o;
+}
